8c25f8add3ec246fa14113fc029abb0a2267e163
dennisExtHaarlem <>
2025-09-03 08:25:42 +0200

    Changes made on 27-08-2025

--- src/main/com/example/AdventOfCode/ListsSorter.java
+++ src/main/com/example/AdventOfCode/ListsSorter.java
@@ -1,11 +1,18 @@
 package main.com.example.AdventOfCode;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ListsSorter {
     public AbstractMap.SimpleEntry<List<Integer>, List<Integer>> sort(AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input)
     {
-        return null;
+        List<Integer> keys = new ArrayList<>(input.getKey());
+        Collections.sort(keys);
+        List<Integer> values = new ArrayList<>(input.getValue());
+        Collections.sort(values);
+
+        return new AbstractMap.SimpleEntry<>(keys, values);
     }
 }
--- src/test/java/com/example/AdventOfCode/SorterTests.java
+++ src/test/java/com/example/AdventOfCode/SorterTests.java
@@ -1,28 +1,47 @@
 package test.java.com.example.AdventOfCode;
 
 import main.com.example.AdventOfCode.ListsSorter;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.AbstractMap;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SorterTests {
-    @Test
-    void SortReturnsEmptyListsGivenEmptyLists(){
-        //Arrange
-        AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input = new AbstractMap.SimpleEntry<List<Integer>,List<Integer>>(
-                new ArrayList<>(),new ArrayList<>()
+    private static Stream<Arguments> InputListsSorter() {
+        return Stream.of(
+                Arguments.of(
+                        //input                                                 //expected
+                        new AbstractMap.SimpleEntry<>(List.of(), List.of()),new AbstractMap.SimpleEntry<>(List.of(), List.of())),
+                Arguments.of(
+                        //input                                                 //expected
+                        new AbstractMap.SimpleEntry<>(List.of(1),List.of(2)),new AbstractMap.SimpleEntry<>(List.of(1), List.of(2))),
+                Arguments.of(
+                        //input                                                 //expected
+                        new AbstractMap.SimpleEntry<>(List.of(1,2),List.of(3,4)),new AbstractMap.SimpleEntry<>(List.of(1,2), List.of(3,4))),
+                Arguments.of(
+                        //input                                                 //expected
+                        new AbstractMap.SimpleEntry<>(List.of(56208,52621),List.of(95668,74203)),new AbstractMap.SimpleEntry<>(List.of(52621,56208), List.of(74203,95668)))
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("InputListsSorter")
+    void SorterReturnsValidValuesGivenValidInput (AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input,
+                                                  AbstractMap.SimpleEntry<List<Integer>,List<Integer>> expected)
+    {
+        // Arrange
         ListsSorter sut = new ListsSorter();
 
-        //Act
+        // Act
         AbstractMap.SimpleEntry<List<Integer>, List<Integer>> result = sut.sort(input);
 
-        //Assert
-        assertTrue(result.getKey().isEmpty());
-        assertTrue(result.getValue().isEmpty());
-
+        // Assert
+        assertEquals(expected, result);
     }
+
 }
